41d375c0dd2c712f167201ea538d044972a85ff2
Tevin Wang <tevinw@boston-cluster.lti.cs.cmu.edu>
2025-01-17 22:57:43 -0500

    server update, range dictionary

--- backend/helpers/range_dictionary.py
+++ backend/helpers/range_dictionary.py
@@ -3,7 +3,7 @@ def create_range_dictionary(file_path):
     with open(file_path, 'r') as file:
         for line in file:
             # Assuming each line contains two numbers separated by a tab
-            start, end = map(int, line.strip().split('\t'))
+            start, end = map(int, line.strip().split())
             # Append the range as a tuple to the list
             range_list.append((start, end))
     return range_list
